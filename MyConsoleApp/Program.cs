54b120fcd0d5743775ab6ff10ff933d2ff98f702
rlesias <rlesias@hotmail.com>
2013-11-16 22:54:13 +0100

    Introduced an error on purpose

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -11,7 +11,7 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            Error
             Console.Read();
         }
     }

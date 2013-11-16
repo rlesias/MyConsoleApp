cbe04d8c786c58aaa3348e81bf292bdef5bf106b
rlesias <rlesias@hotmail.com>
2013-11-16 23:07:38 +0100

    error2 fixed

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -11,11 +11,6 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
-            Error2
-=======
-            Error
->>>>>>> 54b120fcd0d5743775ab6ff10ff933d2ff98f702
             Console.Read();
         }
     }
